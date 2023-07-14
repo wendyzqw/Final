6598d16bb1cea00456b02a5da7933db72ad76d34
wendyzqw <37916754+wendyzqw@users.noreply.github.com>
2023-07-14 08:16:05 +0800

    Create parameters.py

---
+++ TASK2/parameters.py
@@ -0,0 +1,19 @@
+# 训练的次数
+epoch = 3
+ 
+# 训练的批次大小
+batch_size = 100
+ 
+# 数据集的分类类别数量
+num_class = 100
+ 
+# 模型训练时候的学习率大小
+lr = 0.002
+ 
+# 保存模型权重的路径 保存xml文件的路径
+save_path = './res/'
+save_model = './res/best_model.pth'
+ 
+ 
+ 
+ 
